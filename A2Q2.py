ebc208d8072dc18f4dcaae1c3c4b458b6d837cbf
momansethi <104350782+momansethi@users.noreply.github.com>
2022-05-15 22:34:56 +0530

    Add files via upload

---
+++ A2Q2.py
@@ -0,0 +1,7 @@
+#QUESTION_2
+print("QUESTION_2:",end='\n')
+Name='ABC'
+SID=19102034
+Department='XYZ'
+CGPA=9.9
+print("Hey, %s Here! \nMy SID is %d \nI am from %s department and my CGPA is %f." % (Name, SID, Department, CGPA), end='\n')
